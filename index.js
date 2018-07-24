25c027070b84deae59b2c89b0ee6ff94b18c6e56
Brian Zhang <brianzhangw@gmail.com>
2018-07-24 02:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   
-  if
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return katzDeliLine.shift();
+  }
   
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }else{
+    String message = "The line is currently: "
+    
+    for(let i = 0; i < katzDeliLine.length; i++){
+      message += `${i+1}. ${katzDeliLine[i]}, `;
+    }
+    return message.slice(0, katzDeliLine.length-1);
+    
+  }
 }
