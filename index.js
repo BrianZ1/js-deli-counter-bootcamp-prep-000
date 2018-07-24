4d43f40ff3915c210e2ea98710a7dfe217d7e892
Brian Zhang <brianzhangw@gmail.com>
2018-07-24 02:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
-    return "The line is currently empty."
-  }else{
-    String message = "The line is currently: "
+    return "The line is currently empty.";
+  }
+  else{
+    String message = "The line is currently: ";
     
     for(let i = 0; i < katzDeliLine.length; i++){
       message += `${i+1}. ${katzDeliLine[i]}, `;
